5f01abb36a71ddb2c6ef238d71c3c853a55c1a61
Starkill1010 <Baddydog000@gmail.com>
2021-01-03 19:42:30 -0500

    Updated logger file to add to log file.

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,20 @@ import logging
 logger = logging.getLogger('discord')
 
 def setup() :
-  logger.setLevel(logging.DEBUG)
+  """Creates handler to log discord events"""
+  logger.setLevel(logging.INFO)
   handler = logging.FileHandler(filename='history.log', encoding='utf-8', mode='w')
   handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: | %(message)s'))
   logger.addHandler(handler)
+
+def logInfo(message: str):
+  """Logs Message into log at Info level"""
+  logger.info(message)
+
+def logWarn(message: str):
+  """Logs Message into log at Warn level"""
+  logger.warn(message)
+
+def logErr(message: str):
+  """Logs Message into log at Error level"""
+  logger.error(message)
